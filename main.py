7528177d62cf6c5e6c766b985cb6e8274267e42d
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-12-30 11:44:06 +0100

    add basic game mechanics

--- main.py
+++ main.py
@@ -1,8 +1,152 @@
 import random
 
 
+WORDS = ['dog', 'cat', 'hangman']
+
+HANGMAN = {
+    0:
+'''
+
+
+
+
+
+
+''',
+    1:
+'''
+
+
+
+
+
+  _________
+''',
+    2:
+'''
+
+ |
+ |
+ |
+ |
+ |_________
+''',
+    3:
+'''
+ __________
+ |/
+ |
+ |
+ |
+ |_________
+''',
+    4:
+'''
+ __________
+ |/      |
+ |
+ |
+ |
+ |_________
+''',
+    5:
+'''
+ __________
+ |/      |
+ |      ( )
+ |
+ |
+ |_________
+''',
+    6:
+'''
+ __________
+ |/      |
+ |      ( )
+ |       |
+ |
+ |_________
+''',
+    7:
+'''
+ __________
+ |/      |
+ |      ( )
+ |      /|
+ |
+ |_________
+''',
+    8:
+'''
+ __________
+ |/      |
+ |      ( )
+ |      /|\ 
+ |
+ |_________
+''',
+    9:
+'''
+ __________
+ |/      |
+ |      ( )
+ |      /|\ 
+ |      /
+ |_________
+''',
+    10:
+'''
+ __________
+ |/      |
+ |      ( )
+ |      /|\ 
+ |      / \ 
+ |_________
+''',
+}
+
+
+def display_hangman(wrong_guesses):
+    print(HANGMAN[wrong_guesses])
+
+def display_word(word, correct_letters):
+    for letter in word:
+        if ' ' in word:
+            print(' ', end='')
+        if letter in correct_letters:
+            print(letter.upper(), end='')
+        else:
+            print('_', end='')
+    print()
+
+
 def main():
-    pass
+    while True:
+        word = random.choice(WORDS)
+        correct_letters = []
+        wrong_guesses = 0
+        while True:
+            display_hangman(wrong_guesses)
+            display_word(word, correct_letters)
+            if len(correct_letters) == len(word):
+                print('YOU WON!')
+                break
+            elif wrong_guesses == 10:
+                display_hangman(wrong_guesses)
+                print('YOU LOST!')
+                print('The word was', word.upper())
+                break
+            else:
+                guess = input('letter: ')
+            if guess in word:
+                correct_letters.append(guess)
+            else:
+                wrong_guesses += 1
+
+        again = input('Do you want to play again? (Y/N): ')
+        if again.lower() == 'n':
+            break
+
 
 if __name__ == '__main__':
     main()
