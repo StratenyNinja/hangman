73c586afbf92f6eaa8175432731b3be9000fcbc8
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-12-30 12:35:59 +0100

    add displaying of available letters

--- main.py
+++ main.py
@@ -109,6 +109,14 @@ HANGMAN = {
 def display_hangman(wrong_guesses):
     print(HANGMAN[wrong_guesses])
 
+def display_available_letters(guessed_letters):
+    for letter in 'abcdefghijklmnopqrstuvwxyz':
+        if letter in guessed_letters:
+            print('_', end=' ')
+        else:
+            print(letter.upper(), end=' ')
+    print()
+
 def display_word(word, correct_letters):
     for letter in word:
         if ' ' in word:
@@ -123,11 +131,13 @@ def display_word(word, correct_letters):
 def main():
     while True:
         word = random.choice(WORDS)
+        guessed_letters = []
         correct_letters = []
         wrong_guesses = 0
         while True:
             display_hangman(wrong_guesses)
             display_word(word, correct_letters)
+            display_available_letters(guessed_letters)
             if len(correct_letters) == len(word):
                 print('YOU WON!')
                 break
@@ -138,6 +148,8 @@ def main():
                 break
             else:
                 guess = input('letter: ')
+            if guess not in guessed_letters:
+                guessed_letters.append(guess)
             if guess in word:
                 correct_letters.append(guess)
             else:
